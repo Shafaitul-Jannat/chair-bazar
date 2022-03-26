2fbff9557982c7c2f2f8cc697cc09a9e8706b630
Shafaitul-jannat <shafaitul.jannat@gmail.com>
2022-03-26 17:07:28 +0600

    second

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Shop from './components/Shop/Shop'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Chair Bazar</h1>
+      <Shop></Shop>
+      <div className='Ques-Ans'>
+        <div className='ques'>
+          <h4>How react works?</h4>
+          <p>React.js is an accessible Js package that is used to create single-page apps' ui. For web and mobile apps, it's utilized to manage the view layer. </p>
+        </div>
+        <div className='ques'>
+          <h4></h4>
+        </div>
+
+
+      </div>
+
+    </div >
+
+  );
+}
+
+export default App;
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./Cart.css"
+
+const Cart = (props) => {
+    const { cart, chooseAgain, choose1ForMe } = props;
+
+    // let name;
+    // for (const product of cart) {
+    //     name = name + product.name;
+    // }
+    //console.log(cart);
+
+    return (
+        <div className='cart'>
+            <h3>Selected Chair</h3>
+
+            {cart.length === 0 && (
+                <div className='cart-warning'>
+                    <p> Cart is empty </p>
+                </div>
+            )}
+            {cart.map((item) => (
+                // key = item.id
+                < h6 > {item.name}</h6>
+            ))
+            }
+            <button className="choose1ForMe" onClick={() => choose1ForMe()}>Choose 1 for me</button>
+            <button onClick={() => chooseAgain()}>Choose Again</button>
+        </div >
+    );
+};
+
+export default Cart;
