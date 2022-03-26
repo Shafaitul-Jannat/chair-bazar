18343b0b9d77e788781338314bbbdee5061f4388
Shafaitul-jannat <shafaitul.jannat@gmail.com>
2022-03-26 17:39:35 +0600

    sixth

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
         <div className='ques'>
           <h4>Props vs State</h4>
           Props: It is now a standard that Props, also known as Properties, shall not be modified. Components inherit props from their parent that should not be changed within the component. Data travels from the parent to the child in React and React Native.Props are used to create a single component that may be utilized in multiple places throughout an app. The properties can be set by the parent that is calling the component, which may be different in each location.
+          State:A component's state is internal, whereas props are passed to it. In React/React Native, state is utilized to maintain track of information within components. State can be utilized whenever there is data that will change within a component. The interaction of users with components is an excellent example of how state works. Clicking buttons, checking boxes, filling out forms, and other forms of user interaction are all instances of how state might be used within a component.
         </div>
 
 
